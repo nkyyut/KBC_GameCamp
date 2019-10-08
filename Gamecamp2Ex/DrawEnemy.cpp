97ab4c3103597454b75a3db5b994a132cdca2dfb
yoshihidefunakoshi <11807114@stu.kbc.ac.jp>
2019-10-08 13:42:52 +0900

    敵の処理です

---
+++ Gamecamp2Ex/DrawEnemy.cpp
@@ -0,0 +1,17 @@
+//#include "Gamecamp.h"
+#include "DrawEnemy.h"
+
+void DrawEnemy()
+{
+	
+}
+
+void EnemyControl()
+{
+
+}
+
+void BackScrool()
+{
+
+}
---
+++ Gamecamp2Ex/DrawEnemy.h
@@ -0,0 +1,10 @@
+#ifndef _DRAWENEMY_H_
+#define _DRAWENEMY_H_
+
+#include "DxLib.h"
+
+extern void DrawEnemy();
+extern void EnemyControl();
+extern void BackScrool();
+
+#endif
