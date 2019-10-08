c633b9de9af882e475c9d837f33ee16c36a598b7
LAPTOP-LSSKFDV4\nkyyut <nktyut88@gmail.com>
2019-10-08 12:05:49 +0900

    [修正]コンフリクト前のバージョンに戻しました

---
+++ Gamecamp2Ex/Drawtitle.cpp
@@ -0,0 +1,16 @@
+#include "Source.h"
+#include "DrawTitle.h"
+
+void DrawTitle() 
+{
+
+	SetFontSize( _FONTSIZE_S );
+
+	DrawFormatString( 0, 0, 0xffffff, "SPACE TO MAIN" );
+
+	if( opt.Kflg & PAD_INPUT_M )
+	{
+		GAMESTATE = GAME_INIT;
+	}
+
+}
