0ea2382043ed9d96d7ba73867ed1d3502e798bc4
hapizo3 <necosan2019.program@gmail.com>
2019-10-09 11:56:45 +0900

    MousePoint sourcecode update

--- Gamecamp2Ex/MousePoint.h
+++ Gamecamp2Ex/MousePoint.h
@@ -1,1 +1,24 @@
 #pragma once
+
+#include "Wall.h"
+
+class MousePoint
+{
+	public :
+		MousePoint();
+		void GetMouseClick( const Wall *pwall );
+		void PrintMouseClick();
+		//int HitMouseClick( Wall *pwall );
+		bool killFlg;
+		int bmpX;
+		int bmpY;
+		int mpX;
+		int mpY;
+		int dmpX;
+		int dmpY;
+		int clickFlg;
+
+	private :
+		int OldMouseInput;
+		int NowMouseInput;
+};
