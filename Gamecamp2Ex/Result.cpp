afef4662826013e0f2621914da3bd1f5c4bf3c3e
yoshihidefunakoshi <11807114@stu.kbc.ac.jp>
2019-10-10 13:43:46 +0900

    タイトルとリザルトのサウンドの追加

--- Gamecamp2Ex/Result.cpp
+++ Gamecamp2Ex/Result.cpp
@@ -14,8 +14,12 @@ void ResultScene::DrawResult()
 	LoadGraphScreen(0, 0, "Assets/ResultImage.png", false);
 	DrawFormatString(0, 0, 0xffffff,"%d, %d", mpX, mpY);
 	this->backButton.DrawButton();
-
+	
 	this->SceneBack();
+
+	if (CheckSoundMem(GameClearBGM) == 0) {
+		PlaySoundMem(GameClearBGM, DX_PLAYTYPE_NORMAL);
+	}
 
 	if (opt.Kflg & PAD_INPUT_M)
 	{
@@ -37,6 +41,7 @@ ResultScene::~ResultScene()
 void ResultScene::Init()
 {
 	this->backButton.image = LoadGraph("Assets/backButton.png");
+	this->LoadSounds();
 	this->backButton.SetButtonPosition(512,635);
 	this->backButton.SetHitPos(420,600,610,660);
 }
@@ -52,7 +57,17 @@ void ResultScene::SceneBack()
 
 	if (mouse && MOUSE_INPUT_LEFT) {
 		if (backButton.ChackHit(mpX, mpY) == 1) {
+			PlaySoundMem(click, DX_PLAYTYPE_BACK);
 			GAMESTATE = GAME_TITLE;
 		}
 	}
+}
+
+int ResultScene::LoadSounds()
+{
+	if ((GameClearBGM = LoadSoundMem("Assets/ポケットサウンド/wafu-clear.mp3")) == -1) return -1;
+
+	if ((click = LoadSoundMem("Assets/ポケットサウンド/Click.mp3")) == -1)  return -1;
+
+	return 0;
 }
